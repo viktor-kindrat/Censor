3e549ea03d6e9a49d2022cd70dfc3011bc88b85b
Victor Kindrat <kindratvictor07@gmail.com>
2022-02-19 15:09:11 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,38 @@
+let arr = [];
+addWord.onclick = function() {
+    if (forbid.value.length === 0) {
+        addWordGroup.style.animation = "drag 3 0.2s"
+        setTimeout(() => {
+            addWordGroup.style.animation = "none"
+        }, 1000)
+    } else {
+        let info = forbid.value;
+        arr.push(info);
+        if (arr.length === 1) {
+            mas.innerHTML += info;
+            forbid.value = '';
+        } else {
+            mas.innerHTML += ', ' + info;
+            forbid.value = '';
+        }
+    }
+}
+cenzor.onclick = function() {
+    let info = '';
+    info = area.value.toLowerCase();
+    let infoArr = info.split(' ');
+    console.log(infoArr);
+    for (let j = 0; j < arr.length; j++) {
+        for (let i = 0; i < infoArr.length; i++) {
+            if (arr[j] == infoArr[i]) {
+                let star = '';
+                for (let s = 0; s < arr[j].length; s++) {
+                    star += '*';
+                    infoArr[i] = star;
+                }
+            }
+        }
+    }
+    let str = infoArr.join(' ');
+    area.value = str;
+}
